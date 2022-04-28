801ed834d05f1d24592b797b252bfee4bcdb660a
Iliyan Rusev <ilianrusev@gmail.com>
2022-04-28 22:35:25 +0300

    game service added

--- games-play/src/components/GameCatalog/GameCatalog.js
+++ games-play/src/components/GameCatalog/GameCatalog.js
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
 
+import * as gameService from "../../services/gameService";
 import GameCard from "./GameCard";
 
 const GameCatalog = () => {
     const [games, setGames] = useState([]);
 
     useEffect(() => {
-        fetch('http://localhost:3030/data/games?sortBy=_createdOn%20desc')
-            .then((response) => response.json())
+        gameService.getAll()
             .then(result => {
                 setGames(result)
             })
