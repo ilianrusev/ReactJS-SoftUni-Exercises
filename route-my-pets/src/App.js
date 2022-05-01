7d7801944b63dcc5aa61fa299aba1110fc42d253
Iliyan Rusev <ilianrusev@gmail.com>
2022-05-01 15:18:04 +0300

    add user when login

--- route-my-pets/src/App.js
+++ route-my-pets/src/App.js
@@ -1,4 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
+import * as authService from './services/authService'
 import Create from './components/Create/Create';
 import Dashboard from './components/Dashboard/Dashboard';
 import Header from './components/Header/Header';
@@ -7,15 +10,32 @@ import MyPets from './components/MyPets/MyPets';
 import Register from './components/Register/Register';
 
 function App() {
+
+  const [userInfo, setUserInfo] = useState({ isAuthenticated: false, username: '' })
+
+  useEffect(() => {
+    let user = authService.getUser()
+
+    setUserInfo({
+      isAuthenticated: Boolean(user),
+      user,
+    })
+
+  }, [])
+
+  const onLogin = (username) => {
+
+  }
+
   return (
     <div id="container">
 
-      <Header />
+      <Header {...userInfo} />
 
       <main id="site-content">
         <Routes>
           <Route path="/" element={<Dashboard />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/login" element={<Login onLogin={onLogin} />} />
           <Route path="/register" element={<Register />} />
           <Route path="/create" element={<Create />} />
           <Route path="/my-pets" element={<MyPets />} />
--- route-my-pets/src/components/Header/Header.js
+++ route-my-pets/src/components/Header/Header.js
@@ -1,21 +1,36 @@
 import { Link } from "react-router-dom"
 
-const Header = () => {
+const Header = ({
+    isAuthenticated,
+    user
+}) => {
+
+    let guestNavigation = (
+        <div id="guest">
+            <Link className="button" to="/login">Login</Link>
+            <Link className="button" to="/register">Register</Link>
+        </div>
+    )
+
+    let userNavigation = (
+        <div id="user">
+            <span>Welcome, {user}</span>
+            <Link className="button" to="/my-pets">My Pets</Link>
+            <Link className="button" to="/create">Add Pet</Link>
+            <Link className="button" to="/logout">Logout</Link>
+        </div>
+    )
+
     return (
         <header id="site-header">
             <nav className="navbar">
                 <section className="navbar-dashboard">
                     <a href="/">Dashboard</a>
-                    <div id="guest">
-                        <Link className="button" to="/login">Login</Link>
-                        <Link className="button" to="/register">Register</Link>
-                    </div>
-                    <div id="user">
-                        <span>Welcome, email</span>
-                        <Link className="button" to="/my-pets">My Pets</Link>
-                        <Link className="button" to="/create">Add Pet</Link>
-                        <Link className="button" to="/logout">Logout</Link>
-                    </div>
+                    {isAuthenticated
+                        ? userNavigation
+                        : guestNavigation
+                    }
+
                 </section>
             </nav>
         </header>
